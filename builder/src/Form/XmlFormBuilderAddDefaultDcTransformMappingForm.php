7b03482988abf98fbe9a7c9d44598df1cf7e0109
willtp87 <willtp87@hotmail.com>
2017-09-27 12:27:24 -0300

    default xslt admin

--- builder/src/Form/XmlFormBuilderAddDefaultDcTransformMappingForm.php
+++ builder/src/Form/XmlFormBuilderAddDefaultDcTransformMappingForm.php
@@ -1,16 +1,14 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\xml_form_builder\Form\XmlFormBuilderAddDefaultDcTransformMappingForm.
- */
-
 namespace Drupal\xml_form_builder\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Render\Element;
 
+
+/**
+ * Add a default DC transform form.
+ */
 class XmlFormBuilderAddDefaultDcTransformMappingForm extends FormBase {
 
   /**
@@ -20,8 +18,11 @@ class XmlFormBuilderAddDefaultDcTransformMappingForm extends FormBase {
     return 'xml_form_builder_add_default_dc_transform_mapping_form';
   }
 
-  public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
-    module_load_include('inc', 'xml_form_builder', 'includes/db');
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form_state->loadInclude('xml_form_builder', 'inc', 'includes/db');
     $xslts = xml_form_builder_get_xslts();
     $options = [];
     foreach ($xslts as $xslt) {
@@ -29,49 +30,54 @@ class XmlFormBuilderAddDefaultDcTransformMappingForm extends FormBase {
     }
     $form['name'] = [
       '#type' => 'textfield',
-      '#title' => t('Name'),
+      '#title' => $this->t('Name'),
       '#required' => TRUE,
     ];
     $form['content_model'] = [
       '#type' => 'textfield',
-      '#title' => t('Content Model'),
+      '#title' => $this->t('Content Model'),
       '#required' => TRUE,
-      '#autocomplete_path' => ISLANDORA_CONTENT_MODELS_AUTOCOMPLETE,
+      '#autocomplete_route_name' => 'islandora.content_model_autocomplete',
     ];
     $form['DSID'] = [
       '#type' => 'textfield',
       '#required' => TRUE,
-      '#title' => t('DSID'),
+      '#title' => $this->t('DSID'),
     ];
     $form['xslt'] = [
       '#type' => 'select',
       '#options' => $options,
       '#required' => TRUE,
-      '#title' => t('XSLT'),
+      '#title' => $this->t('XSLT'),
     ];
 
     $form['submit'] = [
       '#type' => 'submit',
-      '#value' => t('Add'),
+      '#value' => $this->t('Add'),
     ];
     return $form;
   }
 
-  public function validateForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
-    module_load_include('inc', 'xml_form_builder', 'includes/db');
-    if (xml_form_builder_get_default_transform($form_state->getValue([
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $form_state->loadInclude('xml_form_builder', 'inc', 'includes/db');
+    if (xml_form_builder_get_default_transform($form_state->getValue(
       'content_model'
-      ]), $form_state->getValue(['DSID']))) {
-      $form_state->setErrorByName('content_model', t('There already exists a mapping for this content model and DSID.'));
+      ), $form_state->getValue('DSID'))) {
+      $form_state->setErrorByName('content_model', $this->t('There already exists a mapping for this content model and DSID.'));
       $form_state->setErrorByName('DSID', '');
     }
   }
 
-  public function submitForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
-    module_load_include('inc', 'xml_form_builder', 'includes/db');
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $form_state->loadInclude('xml_form_builder', 'inc', 'includes/db');
     xml_form_builder_add_default_dc_transform_mapping($form_state->getValue(['content_model']), $form_state->getValue(['DSID']), $form_state->getValue(['xslt']), $form_state->getValue(['name']));
-    $form_state->set(['redirect'], XML_FORM_BUILDER_XSLTS_MENU);
+    $form_state->setRedirect('xml_form_builder.xslts_form');
   }
 
 }
-?>
--- builder/src/Form/XmlFormBuilderXsltsForm.php
+++ builder/src/Form/XmlFormBuilderXsltsForm.php
@@ -1,16 +1,13 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\xml_form_builder\Form\XmlFormBuilderXsltsForm.
- */
-
 namespace Drupal\xml_form_builder\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Render\Element;
 
+/**
+ * Default DC transforms form.
+ */
 class XmlFormBuilderXsltsForm extends FormBase {
 
   /**
@@ -20,21 +17,27 @@ class XmlFormBuilderXsltsForm extends FormBase {
     return 'xml_form_builder_xslts_form';
   }
 
-  public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
-    module_load_include('inc', 'xml_form_builder', 'includes/db');
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form_state->loadInclude('xml_form_builder', 'inc', 'includes/db');
 
-    $form['xml_form_builder_xslts_vertical_tabs'] = ['#type' => 'vertical_tabs'];
+    $form['xml_form_builder_xslts_vertical_tabs']['tabs'] = [
+      '#type' => 'vertical_tabs',
+      '#default_tab' => 'xslts',
+    ];
     $form['xml_form_builder_xslts_vertical_tabs']['xslts'] = [
-      '#type' => 'fieldset',
-      '#title' => t('XSLTs'),
-      '#collapsible' => TRUE,
-      '#collapsed' => FALSE,
+      '#type' => 'details',
+      '#title' => $this->t('XSLTs'),
+      '#group' => 'tabs',
+      '#open' => TRUE,
     ];
     $form['xml_form_builder_xslts_vertical_tabs']['dc_transforms'] = [
-      '#type' => 'fieldset',
-      '#title' => t('Default DC Transforms'),
-      '#collapsible' => TRUE,
-      '#collapsed' => FALSE,
+      '#type' => 'details',
+      '#group' => 'tabs',
+      '#title' => $this->t('Default DC Transforms'),
+      '#open' => TRUE,
     ];
 
     $xslts = xml_form_builder_get_xslts();
@@ -60,64 +63,67 @@ class XmlFormBuilderXsltsForm extends FormBase {
       '#type' => 'tableselect',
       '#header' => $xslts_header,
       '#options' => $xslts_options,
-      '#empty' => t('No XSLTs available.'),
+      '#empty' => $this->t('No XSLTs available.'),
     ];
     $form['xml_form_builder_xslts_vertical_tabs']['xslts']['delete'] = [
       '#type' => 'submit',
-      '#value' => t('Delete XSLT'),
+      '#value' => $this->t('Delete XSLT'),
     ];
 
     $form['xml_form_builder_xslts_vertical_tabs']['dc_transforms']['transforms_table'] = [
       '#type' => 'tableselect',
       '#header' => $mappings_header,
       '#options' => $mappings_options,
-      '#empty' => t('No defaults set.'),
+      '#empty' => $this->t('No defaults set.'),
     ];
     $form['xml_form_builder_xslts_vertical_tabs']['dc_transforms']['delete'] = [
       '#type' => 'submit',
-      '#value' => t('Delete Default'),
+      '#value' => $this->t('Delete Default'),
     ];
 
     return $form;
   }
 
-  public function validateForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
-    module_load_include('inc', 'xml_form_builder', 'includes/db');
-    if (in_array('xslts', $form_state->get(['clicked_button', '#array_parents']))) {
-      foreach ($form_state->getValue(['xslts_table']) as $uid => $delete) {
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $form_state->loadInclude('xml_form_builder', 'inc', 'includes/db');
+    if (in_array('xslts', $form_state->getTriggeringElement()['#array_parents'])) {
+      foreach ($form_state->getValue('xslts_table') as $uid => $delete) {
         if ($delete) {
           if (xml_form_builder_xslt_is_a_default($uid)) {
             if (!isset($message_set)) {
-              $form_state->setErrorByName('xslts_table', t('Please delete any transforms using the XSLTs first.'));
+              $form_state->setErrorByName('xslts_table', $this->t('Please delete any transforms using the XSLTs first.'));
               $message_set = TRUE;
             }
           }
         }
       }
-      if (!array_filter($form_state->getValue(['xslts_table']))) {
-        $form_state->setErrorByName('xslts_table', t('Please select an XSLT to delete.'));
+      if (!array_filter($form_state->getValue('xslts_table'))) {
+        $form_state->setErrorByName('xslts_table', $this->t('Please select an XSLT to delete.'));
       }
     }
-    elseif (in_array('dc_transforms', $form_state->get([
-      'clicked_button',
-      '#array_parents',
-    ]))) {
-      if (!array_filter($form_state->getValue(['transforms_table']))) {
-        $form_state->setErrorByName('transforms_table', t('Please select a transform to delete.'));
+    elseif (in_array('dc_transforms', $form_state->getTriggeringElement()['#array_parents'])) {
+      if (!array_filter($form_state->getValue('transforms_table'))) {
+        $form_state->setErrorByName('transforms_table', $this->t('Please select a transform to delete.'));
       }
     }
   }
 
-  public function submitForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
-    module_load_include('inc', 'xml_form_builder', 'includes/db');
-    if (in_array('dc_transforms', $form_state->get(['clicked_button', '#array_parents']))) {
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $form_state->loadInclude('xml_form_builder', 'inc', 'includes/db');
+    if (in_array('dc_transforms', $form_state->getTriggeringElement()['#array_parents'])) {
       foreach ($form_state->getValue(['transforms_table']) as $uid => $delete) {
         if ($delete) {
           xml_form_builder_remove_default_dc_transform_mapping($uid);
         }
       }
     }
-    elseif (in_array('xslts', $form_state->get(['clicked_button', '#array_parents']))) {
+    elseif (in_array('xslts', $form_state->getTriggeringElement()['#array_parents'])) {
       foreach ($form_state->getValue(['xslts_table']) as $uid => $delete) {
         if ($delete) {
           xml_form_builder_remove_xslt($uid);
@@ -127,4 +133,3 @@ class XmlFormBuilderXsltsForm extends FormBase {
   }
 
 }
-?>
