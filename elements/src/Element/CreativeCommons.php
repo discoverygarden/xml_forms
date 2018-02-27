312d97ba81c61926a2d229206e87fa3d408b3760
willtp87 <willtp87@hotmail.com>
2018-02-27 16:42:57 -0400

    work on the cc value callback

--- elements/src/Element/CreativeCommons.php
+++ elements/src/Element/CreativeCommons.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\xml_form_elements\Element;
 
+use Drupal\Core\Url;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Element\FormElement;
 
@@ -20,16 +21,43 @@ class CreativeCommons extends FormElement {
       '#input' => TRUE,
       '#tree' => FALSE,
       '#process' => ['xml_form_elements_creative_commons_process'],
-      '#value_callback' => 'xml_form_elements_creative_commons_value_callback',
     ];
 
     return $info;
   }
 
   /**
+   * Value callback for creative commons element.
    *
+   * Input isn't set in form builder edits. Data from the process function can't
+   * be relied on to be available in value callbacks because Drupal caches before
+   * the element is processed.
+   *
+   * @param array $element
+   *   The element.
+   * @param array $input
+   *   The input of the element.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state.
+   *
+   * @return string
+   *   The license URI.
    */
   public static function valueCallback(&$element, $input, FormStateInterface $form_state) {
+    $form_state->loadInclude('xml_form_elements', 'inc', 'includes/creative_commons');
+    if (isset($input) && $input && Url::fromRoute("<current>")->toString() != 'system/ajax') {
+      $storage = $form_state->getStorage();
+      if (isset($storage['xml_form_elements'][$element['#name']]['license_uri'])) {
+        return $storage['xml_form_elements'][$element['#name']]['license_uri'];
+      }
+      else {
+        $license_fieldset = $input['license_fieldset'];
+        $allow_commercial = $license_fieldset['allow_commercial'];
+        $allow_modifications = $license_fieldset['allow_modifications'];
+        $license_jurisdiction = $license_fieldset['license_jurisdiction'];
+        return xml_form_elements_creative_commons_value($allow_commercial, $allow_modifications, $license_jurisdiction);
+      }
+    }
   }
 
 }
